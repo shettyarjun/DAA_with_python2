d376653fc437a29d2d39e450503b40faed86eb82
Arjun Shetty <104248764+shettyarjun@users.noreply.github.com>
2023-09-05 20:09:18 +0530

    Update (8)NQueens.py

--- design and analysis with python/(8)NQueens.py
+++ design and analysis with python/(8)NQueens.py
@@ -1,14 +1,11 @@
 def is_safe(board, row, col, n):
-    # Check the current column
     for i in range(row):
-        if board[i][col] == 1:
-            return False
-        # Check diagonals
-        if col - (row - i) >= 0 and board[i][col - (row - i)] == 1:
-            return False
-        if col + (row - i) < n and board[i][col + (row - i)] == 1:
+        if board[i][col] == 1 or \
+           (col - (row - i) >= 0 and board[i][col - (row - i)] == 1) or \
+           (col + (row - i) < n and board[i][col + (row - i)] == 1):
             return False
     return True
+
 
 def solve_n_queens(n):
     solutions = []
@@ -23,7 +20,7 @@ def solve_n_queens(n):
                 backtrack(row + 1, board)
                 board[row][col] = 0
 
-    board = [[0] * n for _ in range(n)]  # Create an empty board
+    board = [[0] * n for _ in range(n)]
     backtrack(0, board)
     return solutions
 
@@ -31,7 +28,7 @@ def print_solutions(solutions):
     if solutions:
         for sol in solutions:
             for row in sol:
-                print(' '.join('1' if cell == 1 else '0' for cell in row))
+                print(' '.join(map(str, row)))
             print()
     else:
         print("No solutions found.")
