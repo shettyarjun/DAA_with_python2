04331c6fd03e807100c5607aad34e76d58087e13
Arjun Shetty <104248764+shettyarjun@users.noreply.github.com>
2023-09-05 19:42:50 +0530

    Update (9)GraphColoring.py

--- design and analysis with python/(9)GraphColoring.py
+++ design and analysis with python/(9)GraphColoring.py
@@ -3,15 +3,13 @@ class Graph:
         self.edges = {}
         
     def add_edge(self, s1, s2):
-        # Add connections between subjects
         self.edges.setdefault(s1, []).append(s2)
         self.edges.setdefault(s2, []).append(s1)
         
     def schedule(self):
-        colors = {}  # Dictionary to store assigned colors
+        colors = {}
         for subject, neighbors in self.edges.items():
             used_colors = {colors.get(n, 0) for n in neighbors if n in colors}
-            # Find the minimum unused color for the subject
             colors[subject] = min(set(range(1, len(self.edges) + 1)) - used_colors, default=len(self.edges) + 1)
         return colors
     
@@ -23,14 +21,15 @@ def main():
     
     n = int(input("Enter number of subjects: "))
     for _ in range(n):
-        subject = input("Enter subject: ")
-        num_students = int(input(f"Enter number of students for {subject}: "))
-        students = [input(f"Enter student {j + 1} for {subject}: ") for j in range(num_students)]
+      subjects = [input("Enter subject: ")]
+      m = int(input("Enter number of students: "))
+      for i in range(m):
+        input(f"Enter student {i+1} : ")
     
     num_edges = int(input("Enter number of edges: "))
     for _ in range(num_edges):
-        edge = input("Enter edge (subject1 subject2): ").split()
-        graph.add_edge(edge[0], edge[1])
+        s1, s2 = input("Enter edge (subject1 subject2): ").split()
+        graph.add_edge(s1, s2)
     
     print("Minimum time slots needed:", graph.min_time_slots())
 
