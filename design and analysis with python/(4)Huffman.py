2b263826db3c7ac7e5a6d8ccf40a72403a07796d
Arjun Shetty <104248764+shettyarjun@users.noreply.github.com>
2023-09-06 18:50:25 +0530

    Update (4)Huffman.py

--- design and analysis with python/(4)Huffman.py
+++ design and analysis with python/(4)Huffman.py
@@ -8,66 +8,87 @@ class HuffmanNode:
         self.right = None
 
     def __lt__(self, other):
-        return self.freq < other.freq if isinstance(other, HuffmanNode) else NotImplemented
+        return self.freq < other.freq
 
-def build_huffman_tree(chars, freqs):
-    heap = [HuffmanNode(chars[i], freqs[i]) for i in range(len(chars))]
-    heapq.heapify(heap)
+def build_huffman_tree(characters, frequencies):
+    heap = []
+    for i in range(len(characters)):
+        node = HuffmanNode(characters[i], frequencies[i])
+        heapq.heappush(heap, node)
 
     while len(heap) > 1:
-        left = heapq.heappop(heap)
-        right = heapq.heappop(heap)
-        combined = HuffmanNode(None, left.freq + right.freq)
-        combined.left = left
-        combined.right = right
-        heapq.heappush(heap, combined)
+        left_node = heapq.heappop(heap)
+        right_node = heapq.heappop(heap)
+        combined_freq = left_node.freq + right_node.freq
+        combined_node = HuffmanNode(None, combined_freq)
+        combined_node.left = left_node
+        combined_node.right = right_node
+        heapq.heappush(heap, combined_node)
 
     return heapq.heappop(heap)
 
-def build_huffman_codes(root, code='', codes={}):
-    if root:
-        if root.char:
-            codes[root.char] = code
-        build_huffman_codes(root.left, code + '0', codes)
-        build_huffman_codes(root.right, code + '1', codes)
+def build_huffman_codes(root, current_code, huffman_codes):
+    if root is None:
+        return
 
-def encode_decode_string(encoded, root):
-    decoded, current = '', root
-    for bit in encoded:
-        current = current.left if bit == '0' else current.right
-        if current.char:
-            decoded += current.char
-            current = root
-    return decoded
+    if root.char is not None:
+        huffman_codes[root.char] = current_code
 
-def get_input(count, data_type):
-    inputs = []
-    for i in range(count):
-        val = data_type(input(f"Enter {'char' if data_type == str else 'prob'} {i + 1}: "))
-        inputs.append(val)
-    return inputs
+    build_huffman_codes(root.left, current_code + '0', huffman_codes)
+    build_huffman_codes(root.right, current_code + '1', huffman_codes)
 
-def main():
-    n = int(input("Enter number of chars: "))
-    chars = get_input(n, str)
-    freqs = get_input(n, float)
-    tree = build_huffman_tree(chars, freqs)
-    codes = {}
-    build_huffman_codes(tree, '', codes)
+def encode_string(string, huffman_codes):
+    encoded_string = ''
+    for char in string:
+        encoded_string += huffman_codes[char]
+    return encoded_string
 
-    string = input("Enter a string: ")
-    encoded = ''.join(codes[char] for char in string)
+def decode_string(encoded_string, root):
+    decoded_string = ''
+    current_node = root
 
-    print("Huffman Codes:")
-    for char, code in codes.items():
-        print(char, ":", code)
+    for bit in encoded_string:
+        if bit == '0':
+            current_node = current_node.left
+        else:
+            current_node = current_node.right
 
-    if input("Decode string? (Y/N): ").lower() == 'y':
-        binary = encoded if input("Binary? (Y/N): ").lower() == 'y' else input("Enter binary: ")
-        decoded = encode_decode_string(binary, tree)
-        print("Decoded string:", decoded)
-    else:
-        print("Decoding skipped.")
+        if current_node.char is not None:
+            decoded_string += current_node.char
+            current_node = root
 
-if __name__ == "__main__":
-    main()
+    return decoded_string
+
+n = int(input("Enter the number of characters: "))
+characters = []
+frequencies = []
+
+for i in range(n):
+    char = input("Enter character: ")
+    frequency = float(input("Enter probability: "))
+    characters.append(char)
+    frequencies.append(frequency)
+
+huffman_tree = build_huffman_tree(characters, frequencies)
+huffman_codes = {}
+build_huffman_codes(huffman_tree, '', huffman_codes)
+
+string = input("Enter a string to encode: ")
+encoded_string = encode_string(string, huffman_codes)
+
+print("Huffman Codes:")
+for char, code in huffman_codes.items():
+    print(char, ":", code)
+
+print("Encoded string:", encoded_string)
+
+decode_choice = input("Do you want to decode the encoded string? (Y/N): ")
+if decode_choice.lower() == 'y':
+    decoded_string = decode_string(encoded_string, huffman_tree)
+    print("Decoded string:", decoded_string)
+
+decode_new_choice = input("Do you want to decode a new binary number? (Y/N): ")
+if decode_new_choice.lower() == 'y':
+    binary_number = input("Enter the binary number to decode: ")
+    decoded_new_string = decode_string(binary_number, huffman_tree)
+    print("Decoded string:", decoded_new_string)
