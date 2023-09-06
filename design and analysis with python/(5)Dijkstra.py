a0f19a89e588fd4f22a758b347b76b30d169997b
Arjun Shetty <104248764+shettyarjun@users.noreply.github.com>
2023-09-06 18:45:56 +0530

    Update (5)Dijkstra.py

--- design and analysis with python/(5)Dijkstra.py
+++ design and analysis with python/(5)Dijkstra.py
@@ -1,48 +1,42 @@
-def dijkstra(n, source, cost):
-    v = [0] * (n + 2)
-    d = [float('inf')] * (n + 2)
-    p = [[] for _ in range(n + 2)]
+def dij(n, v, cost):
+    flag = [0 for x in range(1, n + 2, 1)]
+    dist = [float('inf') for x in range(1, n + 2, 1)]
+    dist[v] = 0
+    path = [[] for x in range(1, n + 2, 1)]
+    path[v].append(v)
+    
+    for _ in range(1, n + 1, 1):
+        u = -1
+        min_dist = float('inf')
+        for w in range(1, n + 1, 1):
+            if not flag[w] and dist[w] < min_dist:
+                u = w
+                min_dist = dist[w]
+        if u == -1:
+            break
+        flag[u] = 1
+        for w in range(1, n + 1, 1):
+            if dist[u] + cost[u][w] < dist[w]:
+                dist[w] = dist[u] + cost[u][w]
+                path[w] = path[u] + [w]
+    
+    return dist, path
 
-    d[source] = 0
-    p[source] = [source]
+n = int(input("Enter the number of vertices: "))
+print("Enter the weighted matrix (separated by spaces):")
+cost = [[0 for j in range(1, n + 2, 1)] for i in range(1, n + 2, 1)]
 
-    for _ in range(n):
-        u = min((i for i in range(1, n + 1) if not v[i]), key=lambda i: d[i])
-        v[u] = 1
+for i in range(1, n + 1, 1):
+    row = list(map(int, input().split()))
+    for j in range(1, n + 1, 1):
+        cost[i][j] = row[j - 1]
+        if cost[i][j] == 0:
+            cost[i][j] = float('inf')
 
-        for w in range(1, n + 1):
-            if not v[w]:
-                x = d[u] + cost[u][w]
-                if x < d[w]:
-                    d[w] = x
-                    p[w] = p[u] + [w]
+v = int(input("Enter the source vertex: "))
+dist, path = dij(n, v, cost)
 
-    return d, p
-
-def main():
-    n = int(input("Vertices: "))
-    cost = [[float('inf')] * (n + 2) for _ in range(n + 2)]
-
-    for i in range(1, n + 1):
-        cost[i][1:n + 1] = map(int, input(f"Costs for vertex {i}: ").split())
-
-    source = int(input("Source vertex: "))
-    distances, shortest_paths = dijkstra(n, source, cost)
-
-    print(f"Shortest paths from {source}:")
-    for i in range(1, n + 1):
-        if i != source:
-            print(f"{source} -> {' -> '.join(map(str, shortest_paths[i][1:]))} = {distances[i]}")
-
-if __name__ == "__main__":
-    main()
-# dijkstra: The name of the algorithm.
-# num_vertices: The number of vertices in the graph.
-# source_vertex: The starting vertex for finding shortest paths.
-# edge_costs: The costs or weights of edges in the graph.
-# visited: A list to keep track of visited vertices.
-# distances: A list to store the shortest distances from the source to each vertex.
-# paths: A list to store the shortest paths from the source to each vertex.
-# current_vertex: The current vertex being processed in the algorithm.
-# neighbor_vertex: A vertex adjacent to the current vertex.
-# potential_distance: The potential new distance for a neighbor vertex.
+print("The shortest path from source", v, "to the remaining vertices is:")
+for i in range(1, n + 1):
+    if i != v:
+        print(v, "->", " -> ".join(map(str, path[i][1:])), "=", dist[i])
