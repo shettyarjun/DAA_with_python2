52193dfc9eda91c013c81e6edd411f401abafb62
Arjun Shetty <104248764+shettyarjun@users.noreply.github.com>
2023-09-06 08:35:01 +0530

    Update (2)JobScheduling.py

--- design and analysis with python/(2)JobScheduling.py
+++ design and analysis with python/(2)JobScheduling.py
@@ -5,35 +5,22 @@ class Job:
         self.profit = profit
 
 def schedule_jobs(jobs, T):
-    profit = 0
-    slot = [0] * T
-    
-    # Sort jobs based on profit (highest profit first)
     jobs.sort(key=lambda x: x.profit, reverse=True)
-    
-    # Iterate through each job
+    slot = [0] * T
+    profit = 0
+
     for j in jobs:
-        # Try to find a suitable slot starting from nearest deadline
         for i in range(min(j.deadline - 1, T - 1), -1, -1):
-            if slot[i] == 0:  # If the slot is available
+            if not slot[i]:
                 slot[i] = j.taskid
                 profit += j.profit
-                break  # Job scheduled, break the loop
-    
-    # Print scheduled jobs and total profit
+                break
+
     print("Scheduled Jobs:", [x for x in slot if x != 0])
     print("Total Profit:", profit)
 
 if __name__ == "__main__":
     num_jobs = int(input("Enter number of jobs: "))
-    
-    # Collect job details from the user
-    jobs = []
-    for i in range(num_jobs):
-        task_id = int(input(f"Task ID for Job {i + 1}: "))
-        deadline = int(input(f"Deadline for Job {i + 1}: "))
-        profit = int(input(f"Profit for Job {i + 1}: "))
-        jobs.append(Job(task_id, deadline, profit))
-    
+    jobs = [Job(*map(int, input(f"Task ID, Deadline, Profit for Job {i + 1}: ").split())) for i in range(num_jobs)]
     deadline_limit = int(input("Enter Deadline Limit: "))
     schedule_jobs(jobs, deadline_limit)
